c377afb757fc49de431c5ab405622cb941a93081
Diego Mayorga <diegomayorga@MacBook-Air-de-Diego.local>
2025-07-24 19:21:38 -0400

    se agrega cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const productosRoutes = require('./routes/productos');
 const pedidosRoutes = require('./routes/pedidos');
 
 const app = express();
 
 // Middlewares
+app.use(cors()); // Habilita CORS para todas las rutas y orígenes
 app.use(express.json()); // Para entender los cuerpos de las peticiones en formato JSON
 
 // Rutas
@@ -12,4 +14,3 @@ app.use('/productos', productosRoutes);
 app.use('/pedido', pedidosRoutes);
 
 module.exports = app;
-
